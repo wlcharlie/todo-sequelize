c9b7e04aba7d41a6f2b86c92bccc49ad5695e2ab
wlcharlie <wl00606352@gmail.com>
2021-06-12 21:23:10 +0800

    feat: add login via Facebook

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const usePassport = require('./config/passport')
+require('dotenv').config()
 
 const routes = require('./routes')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const thirdPartyAuth = require('./modules/thirdPartyAuth')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
+router.use('/auth', thirdPartyAuth)
 router.use('/todos', authenticator, todos)
 router.use('/', authenticator, home)
 
